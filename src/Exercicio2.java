b955f1482543caeea0cdd0334972140f4777f24c
Jéssica <ferrsjessy@gmail.com>
2024-02-24 16:34:05 -0300

    Exercício resolvido - Estrutura Sequencial

---
+++ src/Exercicio2.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+/*
+Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.
+
+- Fórmula da área: area = π . raio2
+- Considere o valor de π = 3.14159
+*/
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double raio, area, pi = 3.14159;
+
+        raio = sc.nextDouble();
+
+        area = pi * raio * raio;
+
+        System.out.printf("A = %.4f\n", area);
+
+        sc.close();
+
+    }
+}
